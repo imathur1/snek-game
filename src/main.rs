72b50005dcc238ebd6ea39b9317e929561fa7b38
Ishaan Mathur <imathur3@illinois.edu>
2021-11-17 17:31:38 -0600

    Added client and server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+use laminar::{ErrorKind};
+
+mod server;
+mod client;
+
+fn main() -> Result<(), ErrorKind> {
+    let stdin = stdin();
+    println!("Please type in `server` or `client`.");
+
+    let mut s = String::new();
+    stdin.read_line(&mut s)?;
+    if s.starts_with('s') {
+        println!("Starting server..");
+        server::server()
+    } else {
+        println!("Starting client..");
+        client::client()
+    }
 }
